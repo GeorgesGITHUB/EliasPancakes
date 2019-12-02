0957d53b847001d7f5dea2ea3e03543598761fca
unknown <georgesatallahbusiness@gmail.com>
2019-12-01 23:29:33 -0500

    Added img slider + placeholders + colour palette

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+  //Initialization
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {"height" : 535, "indicators" : true });
   });
