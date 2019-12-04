fea9cd705649eba1589ddf11ccd2513e1486ddc8
unknown <georgesatallahbusiness@gmail.com>
2019-12-03 21:02:43 -0500

    more semantic elems, product page w/ collapsable

--- js/main.js
+++ js/main.js
@@ -8,3 +8,8 @@
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, {"height" : 535, "indicators" : true });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+  });
